6179c01a7996c0d64e5cbe36eaca89fc4943527e
Mikko Talonen <mtalonen@gmail.com>
2022-12-17 16:56:44 +0200

    adds nugetnagets page

---
+++ pages/nugetsnagets/index.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import type { NextPage } from 'next'
+
+import Image from 'next/image';
+
+import styled from 'styled-components';
+
+const App = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: black;
+  padding-top: 10vh;
+  font-family: sans-serif;
+`
+
+const Layout = styled.div`
+  display: flex;
+  align-self: center;
+  justify-content: center;
+  gap: 8px;
+`;
+
+const Card = styled.div`
+  display: flex;
+  border-radius: 2vh;
+  flex-direction: column;
+  gap: 8px;
+  padding: 8px;
+  height: 80vh;
+  width: 60vh;
+  background-color: white;
+`;
+
+const CardHeader = styled.div`
+  display: flex;
+  justify-content: end;
+  padding: 8px;
+  font-size: 5vh;
+`;
+
+const CardContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 60vh;
+  padding: 8px;
+  font-size: 40vh;
+`;
+
+const CardFooter = styled.div`
+  margin-top: auto;
+  display: flex;
+  justify-content: end;
+  padding: 8px;
+  font-size: 5vh;
+`;
+
+
+const NugetsApp: NextPage = () => {
+
+  return (
+    <App>
+      <Layout>
+        <Card>
+          <CardHeader>290+</CardHeader>
+          <CardContent><Image src='/happokalevi.jpeg' height='500' width='500' /></CardContent>
+          <CardFooter>Happo Kalevi</CardFooter>
+        </Card>
+      </Layout>
+    </App>
+  );
+}
+
+export default NugetsApp;
